6370d55ed85058b2e19fb428e04038d10da313f1
fengtaiguang <taijiec@163.com>
2016-12-22 20:07:16 +0800

    ov

--- photoalbum-infrastructure/src/test/java/com/fengtaiguang/photoalbum/domain/repository/hibernate/HibernateBaseRepositoryTest.java
+++ photoalbum-infrastructure/src/test/java/com/fengtaiguang/photoalbum/domain/repository/hibernate/HibernateBaseRepositoryTest.java
@@ -3,26 +3,35 @@ package com.fengtaiguang.photoalbum.domain.repository.hibernate;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 import com.fengtaiguang.ddd.framwork.domain.repository.IRepository;
+import com.fengtaiguang.photoalbum.domain.entity.Album;
 
 @ContextConfiguration(locations = "classpath*:spring-*.xml")
 public class HibernateBaseRepositoryTest extends AbstractJUnit4SpringContextTests {
 	@Inject
 	@Named("daoTemplate")
 	IRepository daoTemplate;
+	private Logger logger = Logger.getLogger(this.getClass());
 
 	@Before
 	public void setUp() throws Exception {
 	}
+	@Test
+	public void testFindByExample() {
+		Album album=new Album();
+		album.setName("xc");
+		daoTemplate.findByExample(album);
+	}
 
 	@Test
 	public void testSave() {
-		System.out.println(daoTemplate);
+
 	}
 
 	@Test
@@ -37,12 +46,8 @@ public class HibernateBaseRepositoryTest extends AbstractJUnit4SpringContextTest
 
 	@Test
 	public void testGet() {
-
+	
 	}
 
-	@Test
-	public void testFindByExample() {
-
-	}
-
+	
 }
